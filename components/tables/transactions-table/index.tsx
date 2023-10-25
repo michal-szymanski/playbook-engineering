38297d65eeaaaf789d8ce1b9ce9d9fc64090753f
Michał Szymański <michal.szymanski92@gmail.com>
2023-10-25 20:55:57 +0200

    Sum

--- components/tables/transactions-table/index.tsx
+++ components/tables/transactions-table/index.tsx
@@ -1,7 +1,6 @@
 import { DataTable } from '@/components/ui/data-table';
 import { ColumnDef } from '@tanstack/react-table';
-import { z } from 'zod';
-import { transactionSchema } from '@/types';
+import { Transaction } from '@/types';
 import { Button } from '@/components/ui/button';
 import { observer } from 'mobx-react-lite';
 import { useTransactionsStore } from '@/hooks';
@@ -9,7 +8,7 @@ import { useTransactionsStore } from '@/hooks';
 const TransactionsTable = () => {
     const { transactions, conversionRate, removeTransaction } = useTransactionsStore();
 
-    const columns: ColumnDef<z.infer<typeof transactionSchema>>[] = [
+    const columns: ColumnDef<Transaction>[] = [
         {
             id: 'title',
             header: () => <div className="text-left">Title</div>,
@@ -37,7 +36,17 @@ const TransactionsTable = () => {
             )
         }
     ];
-    return <DataTable columns={columns} data={transactions} />;
+
+    const totalPLN = transactions.reduce((acc, curr) => acc + curr.amountPLN, 0);
+
+    return (
+        <div className="flex flex-col gap-5">
+            <DataTable columns={columns} data={transactions} />
+            <span className="text-lg">
+                Sum: {totalPLN} PLN ({(totalPLN * conversionRate).toFixed(2)} EUR)
+            </span>
+        </div>
+    );
 };
 
 export default observer(TransactionsTable);
