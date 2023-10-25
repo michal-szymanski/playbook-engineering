a1d7c16a1a46b2c3c6b1ed1d683febf6d7b32627
Michał Szymański <michal.szymanski92@gmail.com>
2023-10-25 23:25:57 +0200

    Cleaning

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,26 @@ import { Inter } from 'next/font/google';
 import AddTransactionForm from '@/components/forms/add-transaction-form';
 import TransactionsTable from '@/components/tables/transactions-table';
 import SetConversionRateForm from '@/components/forms/set-conversion-rate-form';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
-        <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
-            <div className="container flex flex-col gap-20">
-                <div className="flex items-center justify-between pb-10">
-                    <h1 className=" text-4xl font-bold">List of expenses</h1>
-                    <SetConversionRateForm />
+        <>
+            <Head>
+                <title>Playground Engineering</title>
+            </Head>
+            <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+                <div className="container flex flex-col gap-20">
+                    <div className="flex items-center justify-between pb-10">
+                        <h1 className=" text-4xl font-bold">List of expenses</h1>
+                        <SetConversionRateForm />
+                    </div>
+                    <AddTransactionForm />
+                    <TransactionsTable />
                 </div>
-                <AddTransactionForm />
-                <TransactionsTable />
-            </div>
-        </main>
+            </main>
+        </>
     );
 }
