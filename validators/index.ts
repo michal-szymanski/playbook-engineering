490ae69cb5020c27311d8a50692292b6c2294b0d
Michał Szymański <michal.szymanski92@gmail.com>
2023-10-25 23:15:07 +0200

    Unit tests, rename precision to scale

--- validators/index.ts
+++ validators/index.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
-export const validateCurrencyInput = (value: string, precision: number) => {
+export const validateCurrencyInput = (value: string, scale: number) => {
     const groups = z.array(z.string()).min(0).max(2).parse(value.split('.'));
 
     if (groups.length === 2) {
-        z.string().max(precision).parse(groups[1]);
+        z.string().max(scale).parse(groups[1]);
     }
 
-    z.coerce.number().parse(value);
+    z.coerce.number().positive().parse(value);
 };
